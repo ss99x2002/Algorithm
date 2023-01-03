27a263a8fc33e7b0f7b9aba6f0feeed48e34b2c8
신서현 <70602631+ss99x2002@users.noreply.github.com>
2023-01-03 23:47:50 +0900

    [Gold IV] Title: 오큰수, Time: 220 ms, Memory: 12496 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int N, A;
+int a[1000001];
+int ans[1000001];
+stack<int> stk;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> N; 
+	
+	for (int i =0; i<N; i++)
+	{
+		cin >> A;
+		a[i] = A;
+	}
+
+	for (int i = N-1; i>=0; i--)
+	{
+		while (!stk.empty() && stk.top() <= a[i])
+		{
+			stk.pop();
+		}
+		if (stk.empty())
+		{
+			ans[i] = -1;
+		}
+		else {
+			ans[i] = stk.top();
+		}
+		stk.push(a[i]);
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << ans[i] << " ";
+	}
+}
