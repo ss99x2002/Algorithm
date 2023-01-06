3b198010f4714508ca550607fbdcbfc4ae877007
신서현 <70602631+ss99x2002@users.noreply.github.com>
2023-01-06 23:53:07 +0900

    [Silver IV] Title: 회전하는 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int N, M, A, cnt = 0;
+	int idx = -1;
+	cin >> N >> M;
+	deque<int>deq;
+	vector<int> vec;
+	queue<int> q;
+
+	for (int i = 1; i <=N; i++)
+	{
+		deq.push_back(i);
+	}
+	for (int i = 0; i < M; i++)
+	{
+		cin >> A;
+		q.push(A);
+	}
+	while (!q.empty())
+	{
+		for (int i = 0; i < deq.size(); i++)
+		{
+			if (deq[i] == q.front())
+			{
+				idx = i;
+				break;
+			}
+		}
+
+		if (idx < deq.size()-idx)
+		{
+			while (1)
+			{
+				if (deq.front() == q.front())
+				{
+					deq.pop_front();
+					q.pop();
+					break;
+				}
+				cnt++;
+				deq.push_back(deq.front());
+				deq.pop_front();
+			}
+		}
+		else
+		{
+			while (1)
+			{
+				if (deq.front() == q.front())
+				{
+					deq.pop_front();
+					q.pop();
+					break;
+				}
+				cnt++;
+				deq.push_front(deq.back());
+				deq.pop_back();
+			}
+		}
+	}
+	cout << cnt;
+}
