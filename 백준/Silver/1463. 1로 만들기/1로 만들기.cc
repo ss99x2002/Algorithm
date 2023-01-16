d7fe6f7fc1e488a2b9112da49908344ed0477a7a
신서현 <70602631+ss99x2002@users.noreply.github.com>
2023-01-16 19:44:26 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -2,37 +2,18 @@
 #include <queue>
 #include <algorithm>
 using namespace std;
-
-#define X first;
-#define Y second;
-int vis[1000001];
-
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	int N;
 	cin >> N;
-	fill(vis, vis + 1000001, -1);
-	queue<int> q;
-	q.push(1);
-	vis[1] = 0;
-	while (!q.empty()) {
-		int cur = q.front();
-		q.pop();
-		if (cur == N) {
-			cout << vis[cur];
-			return 0;
-		}
-		for (int dir : {cur * 3, cur * 2, cur + 1}) {
-			if (dir == N) {
-				cout << vis[cur] + 1;
-				return 0;
-			}
-			if ( dir>N || vis[dir]>=0) continue;
-			vis[dir] = vis[cur] + 1;
-			q.push({ dir });
-		}
+	int* arr = new int[N];
+	arr[1] = 0;
+	for (int i = 2; i <= N; i++) {
+		arr[i] = arr[i - 1] + 1;
+		if (i % 2 == 0) arr[i] = min(arr[i], arr[i / 2] + 1);
+		if (i % 3 == 0) arr[i] = min(arr[i], arr[i / 3] + 1);
 	}
-	
+	cout << arr[N];
 }
