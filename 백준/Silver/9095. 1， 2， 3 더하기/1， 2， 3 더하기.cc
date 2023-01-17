b836ae64ecbb4a2abced095077bd6ef31fd7403f
신서현 <70602631+ss99x2002@users.noreply.github.com>
2023-01-17 20:15:35 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -6,28 +6,17 @@ int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int T,N, cnt=0;
+	int T, N;
 	cin >> T;
-	for (int i = 0; i < T; i++)
-	{
+	for (int i = 0; i < T; i++) {
 		cin >> N;
-		cnt =0;
 		int* arr = new int[N + 1];
-		fill(arr, arr + N + 1, 0);
-		queue<int> q;
-		q.push(0);
-		while (!q.empty()) {
-			int cur = q.front();
-			q.pop();
-			if (cur == N) {
-				cnt++;
-			}
-			for (int dir : {cur + 1, cur + 2, cur + 3}) {
-				if (dir<0 || dir>N) continue;
-				arr[dir] = arr[cur] + 1;
-				q.push(dir);
-			}
+		arr[1] = 1;
+		arr[2] = 2;
+		arr[3] = 4;
+		for (int i = 4; i <= N; i++) {
+			arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
 		}
-		cout << cnt << "\n";
+		cout << arr[N] << "\n";
 	}
 }
