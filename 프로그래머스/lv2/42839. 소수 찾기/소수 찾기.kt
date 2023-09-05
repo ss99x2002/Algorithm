b5a8afb5487f8e42b1488c6b6176530c76905e5b
신서현 <70602631+ss99x2002@users.noreply.github.com>
2023-09-05 18:45:04 +0900

    [level 2] Title: 소수 찾기, Time: 47.57 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.kt
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.kt
@@ -1,34 +1,49 @@
+import java.lang.Math.*
+
 class Solution {
     fun solution(numbers: String): Int {
-        var answer = 0
-        val numSet = HashSet<Int>()
-       
-        makeNumberSet("", numbers, numSet)
-        
-        while(numSet.iterator().hasNext()){
-          
-            val num = numSet.iterator().next()
-            numSet.remove(num)
-            
-            if(isPrime(num)) answer++
+        var numberList: MutableList<Int> = mutableListOf()
+        numbers.forEach {
+            numberList.add(it.toString().toInt())
         }
-        
+
+        numberList.sortByDescending {
+            it
+        }
+
+        var number = ""
+        numberList.forEach {
+            number += it.toString()
+        }
+        var answer = 0
+        for (i in 1..number.toInt()) {
+            if (isPrime(i)) {
+                var numString = i.toString()
+                var numListCopy = numberList.toMutableList()
+                var idx = 0
+                for (j in numString.indices) {
+                    for (k in numListCopy.indices) {
+                        if (numString[j].toString().toInt() == numListCopy[k]) {
+                            numListCopy[k] = -1
+                            idx++
+                            break
+                        }
+                    }
+                }
+                if (idx == numString.length) {
+                    answer++
+                }
+            }
+
+        }
         return answer
     }
-    
-    fun makeNumberSet(prefix:String, numbers:String, numSet:HashSet<Int>){
-       
-        if(prefix != "") numSet.add(prefix.toInt())
-        for(i in numbers.indices){
-           
-            makeNumberSet("$prefix${numbers[i]}", numbers.substring(0, i) + numbers.substring(i+1), numSet)
-        }
-    }
-    
-    fun isPrime(num:Int):Boolean{
-       
-        if(num <= 1) return false
 
-        return (2..Math.sqrt(num.toDouble()).toInt()).none{ num % it == 0 }
+    fun isPrime(number: Int): Boolean {
+        if (number == 0 || number == 1) return false
+            for (i in 2..sqrt(number.toDouble()).toInt()) {
+                if (number % i == 0) return false
+            }
+        return true
     }
 }
