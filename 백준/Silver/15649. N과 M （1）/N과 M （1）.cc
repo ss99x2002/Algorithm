cffd7fa6c4d038c1c25acf8558c800441eb255c0
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2025-02-14 13:36:31 +0900

    [Silver III] Title: N과 M (1), Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,29 +1,32 @@
 #include <iostream>
 #include <algorithm>
-using namespace std;
-int n, m;
-int arr[10];
-bool isused[10];
 
-void func(int k) {
-	if (k == m) {
-		for (int i = 0; i < m; i++) {
-			cout << arr[i] << " ";
-		}
-		cout << "\n";
-		return;
-	}
-	for (int i = 1; i <= n; i++) {
-		if (!isused[i]) {
-			arr[k] = i;
-			isused[i] = 1;
-			func(k + 1);
-			isused[i] = 0;
-		}
-	}
+using namespace std;
+
+int n, m;
+int num[10];
+int visited[10];
+
+
+void visitedNumber(int cnt) {
+    if (cnt == m) {
+        for (int i = 0; i < m; i++) {
+            cout << num[i] << " ";
+        }
+        cout << "\n";
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (visited[i] != 1) {
+            visited[i] = 1;
+            num[cnt] = i;
+            visitedNumber(cnt + 1);
+            visited[i] = 0;
+        }
+    }
 }
-int main()
-{
-	cin >> n >> m;
-	func(0);
+
+int main() {
+    cin >> n >> m;
+    visitedNumber(0);
 }
