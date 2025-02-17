dad8574f75e64e5330ce2c02efa954cd489809ca
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2025-02-17 13:35:29 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int input;
+vector<int> num;
+int visited[9];
+int ans[9];
+
+//n개의 수와 m개를 고르는 수열
+//num 에는 N개의 수가 담김 (배열 되어있는 숫자가 아님)
+
+
+void checkBackTracking(int start, int cnt) {
+    if (cnt == m) {
+        for (int i = 0; i < m; i++) {
+            cout << ans[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = start; i < n; i++) {
+        ans[cnt] = num[i];
+        checkBackTracking(i + 1, cnt + 1);
+    }
+
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> input;
+        num.push_back(input);
+    }
+
+    sort(num.begin(), num.end());
+    checkBackTracking(0, 0);
+}
