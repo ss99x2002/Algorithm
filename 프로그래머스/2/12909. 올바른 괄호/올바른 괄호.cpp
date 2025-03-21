d9f4557373e300e9b799d50d8aed91173eb9592a
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2025-03-21 16:18:50 +0900

    [level 2] Title: 올바른 괄호, Time: 0.57 ms, Memory: 3.93 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,35 @@
+#include<string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+stack<char> st;
+
+
+bool solution(string s)
+{
+    bool answer = true;
+
+    for (int i=0; i<s.size(); i++){
+        if (s[i] == '('){
+            st.push(s[i]);
+        }
+        else if (s[i] == ')'){
+            if (!st.empty() && st.top() == '(') {
+             st.pop();  
+            }
+            else {
+                st.push(s[i]);
+            }
+        }
+    }
+    
+    if (st.empty()){
+        answer = true;
+    } else {
+        answer = false;
+    }
+
+    return answer;
+}
