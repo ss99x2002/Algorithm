bd9519dc7d8c342840f73e4f90f2420a7365a52d
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2025-05-14 15:45:18 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.50 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -3,53 +3,57 @@
 #include <queue>
 #include <algorithm>
 
+
 using namespace std;
 
-// 우선순위 디스크 컨트롤러 작동방식
-// 작업 번호, 요청시각, 소요시간 저장해두는 대기 큐 있음 -> 처음에 비어있다.
-// 하드디스크 작업 x, 대기큐 원소 있으면 -> 우선순위 높은거 대기큐에서 꺼내서 작업시킴
+// 입력 예시
+//[[0, 3], [1, 9], [3, 5]]
+// (작업 시작 시간, 작업 소요 시간)
 
-// (우선순위) 소요시간이 짧은것 > 작업 요청 시간이 빠른것 > 작업 번호가 작은것 
-
-// 작업 하나 시작하면 마칠 때까지 그 작업만 수행
-// 
-// 각 작업에 대한 [작업 요청 시점, 작업 소요시간] = jobs
-// 모든 요청 작업 시간의 평균 return solution 
-
-
-priority_queue <pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-
-int solution(vector<vector<int>> jobs) {
-    sort(jobs.begin(), jobs.end());
-    int answer = 0;
-    int i = 0;
-    int sum = 0;
-    int times = 0;
-    int count = 0;
-    while(count< jobs.size() ){
-        while(i < jobs.size() && jobs[i][0] <= times) {
-            pq.push({jobs[i][1], jobs[i][0]});
-            i ++;
+struct cmp {
+    bool operator()(const pair<int,int> a, const pair<int,int> b) const {
+        if(a.second != b.second){
+            return a.second > b.second;
         }
-
-        if (!pq.empty()){
-            times = pq.top().first + times;
-            sum = sum + (times - pq.top().second);
-            pq.pop();
-            count ++;
-        }
-        else {
-            times = jobs[i][0];
-        }
+        return a.first > b.first;
     }
-     answer = sum/(jobs.size());
-    return answer;
+};
+
+
+bool jobsComp(vector<int> a, vector<int> b){
+    return a[0] < b[0];
 }
 
+int solution(vector<vector<int>> jobs) {
+    int answer = 0;
+    int idx =0;
+    int curTime = 0;
+    
+    priority_queue<pair<int,int>, vector<pair<int,int>>, cmp> pq;
+    // pq의 비교함수 주는 방법 다시 보기!
+    
+    sort(jobs.begin(), jobs.end(), jobsComp);
 
-/* 
-1. jobs를 요청 시간 기준으로 정렬
-2. 현재 실행 중인 작업이 끝날 때까지 들어오는 작업을 우선순위 큐에 넣기
-3. 우선순위 큐에서 가장 짧은 작업을 선택하여 수행
-4. 작업 수행 후, 현재 시간을 업데이트
-5. 모든 작업이 끝날 때까지 반복 */ 
+    while(idx < jobs.size() || !pq.empty()){
+        // 대기열 비어있으면, 다음 도착 시간으로 점프
+        if (pq.empty()){
+           curTime = max(curTime, jobs[idx][0]); 
+        }
+        
+        // 지금 시각까지 도착한 작업 모두 대기열에 추가
+        // 조건 따지지 않고 추가하면 된다 -> 나오는 작업 조건은 pq comp에 정의해둠.
+        while(idx < jobs.size() && jobs[idx][0] <= curTime){
+           int arrive = jobs[idx][0];
+           int duration = jobs[idx][1];
+           pq.push({arrive,duration});
+           idx ++;
+        }   
+        // 우선순위 높은 작업 선택 후, 해당 작업이 기다린 시간 계산 및 처리 
+        auto [arrive, duration]  = pq.top();
+        pq.pop();
+        curTime = curTime + duration;
+        answer = answer + (curTime - arrive);
+    }
+    
+    return (int)answer/jobs.size();
+}
