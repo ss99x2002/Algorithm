8d5536e6142649a3af440e1e6549e5c92d064a90
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2025-06-05 13:31:26 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,17 +1,21 @@
 -- C# 이나 Python 스킬 가진 개발자 정보 조회
 -- 개발자 id, 이메일, 이름, 성 조회 SQL
 -- id 기준 ASC
-
 -- ⭐️ WITH 연달아 2개 사용할 수 없음.
+-- 어떤 스킬코드를 갖고있다고 하면, &했을때 1이상으로 존재하므로, >0으로 비교하는 것 잊지말기. 
 
-WITH SKILLS as (
-    SELECT CODE, NAME
+WITH C_SHOP AS (
+    SELECT CODE as SKILL_CODE
     FROM SKILLCODES
-    WHERE NAME IN ('Python', 'C#')
-) 
+    WHERE NAME = 'C#'
+), PYTHON AS (
+    SELECT CODE as SKILL_CODE
+    FROM SKILLCODES
+    WHERE NAME = 'Python'
+)
 
-SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
-FROM DEVELOPERS
-WHERE SKILL_CODE & (SELECT CODE FROM SKILLS WHERE NAME = 'Python') > 0 OR
-SKILL_CODE & (SELECT CODE FROM SKILLS WHERE NAME = 'C#') > 0
-ORDER BY ID ASC
+SELECT d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+FROM DEVELOPERS as d, C_SHOP as c, PYTHON as p 
+WHERE d.SKILL_CODE & c.SKILL_CODE  > 0 OR d.SKILL_CODE & p.SKILL_CODE > 0
+ORDER BY d.ID ASC
+
