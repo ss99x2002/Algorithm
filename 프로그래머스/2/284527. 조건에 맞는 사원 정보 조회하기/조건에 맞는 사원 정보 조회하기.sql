de9f840ea01627dd6ed3496ff94519ed6ba325e3
Seohyeon Shin <70602631+ss99x2002@users.noreply.github.com>
2025-07-02 14:14:09 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,20 +1,29 @@
+-- * 꼭 다시 풀어보기 (25-07-02)
 -- 2022년도 한해 평가 점수 가장 높은 사원 정보 조회
 -- 가장 높은 사원들의 점수, 사번, 성명, 직책, 이메일 조회
 -- 평가 점수 = (상 + 하반기 점수), 평가 점수 as SCORE
 
-SELECT SUM(hg.SCORE) as SCORE, he.EMP_NO as EMP_NO, he.EMP_NAME as EMP_NAME,
-he.POSITION as POSITION, he.EMAIL as EMAIL
-FROM HR_EMPLOYEES as he JOIN HR_GRADE as hg
-ON he.EMP_NO = hg.EMP_NO
-GROUP BY he.EMP_NO, he.EMP_NAME
-HAVING SCORE = (
-    SELECT MAX(SCORE_SUM)
-    FROM (
-          SELECT SUM(hg2.SCORE) AS SCORE_SUM
-    FROM HR_EMPLOYEES AS he2
-    JOIN HR_GRADE AS hg2
-    ON he2.EMP_NO = hg2.EMP_NO
-    GROUP BY he2.EMP_NO
-  ) as subT
+WITH E_SCORE as (
+    SELECT he.emp_no, sum(hg.score) as score 
+    FROM HR_EMPLOYEES as he JOIN HR_GRADE as hg
+    ON he.emp_no = hg.emp_no
+    WHERE hg.year = '2022'
+    GROUP BY he.emp_no
+    ORDER BY sum(hg.score) DESC
+), MAX_SCORE as (
+    SELECT max(score)
+    FROM E_SCORE
 )
-ORDER BY he.EMP_NO ASC
+
+SELECT sum(hg.score) as SCORE,
+he.emp_no as EMP_NO,
+he.emp_name as EMP_NAME,
+he.position as POSITION,
+he.email as EMAIL
+FROM HR_EMPLOYEES as he JOIN HR_GRADE as hg
+ON he.emp_no = hg.emp_no
+GROUP BY he.emp_no, he.emp_name, he.position, he.email
+HAVING sum(hg.score) = (
+       SELECT max(score)
+       FROM E_SCORE
+)
